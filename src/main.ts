04c00f5cad583e249e461400010a3ee14811c6fe
ksunleesh <ksunleesh@gmail.com>
2026-07-22 12:36:36 +0700

    chore: init Nest.js project

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap().catch((error) => {
+  const logger = new Logger('Bootstrap');
+  logger.error('Application failed to start', error);
+  process.exit(1);
+});
