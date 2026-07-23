9529f6181ba8815a133abefcdd8c4d2aedfddfc7
ksunleesh <ksunleesh@gmail.com>
2026-07-23 15:52:32 +0700

    chore: add useGlobalPipes

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true
+    })
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap().catch((error) => {
